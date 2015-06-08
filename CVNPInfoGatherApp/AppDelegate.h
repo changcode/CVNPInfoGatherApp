5332dd1aa17be576c28f6b847aef4355efa8f795
Chang Shu <cshu1@kent.edu>
2015-06-08 00:20:26 -0400

    Initial Commit

---
+++ CVNPInfoGatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CVNPInfoGatherApp
+//
+//  Created by Chang on 6/8/15.
+//  Copyright (c) 2015 Kent State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
